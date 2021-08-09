0a47fa816cb829086b10a13fbc8248e058bf411f
Mohetti <moritz.hettich@gmx.de>
2021-08-09 15:54:53 +0300

    First Commit

---
+++ client/src/interfaces.ts
@@ -0,0 +1,41 @@
+export interface HeroListForStartingItems {
+  [key: string]: StartingItems;
+}
+
+interface StartingItems {
+  item1: number | null;
+  item2: number | null;
+  item3: number | null;
+  item4: number | null;
+  item5: number | null;
+  item6: number | null;
+}
+
+export interface ItemListInterface {
+  [key: string]: {
+    relevantValues: boolean;
+    gold: number;
+    strBonus?: number;
+    agiBonus?: number;
+    intBonus?: number;
+    hpRaw?: number;
+    hpRegenPermanent?: number;
+    hpRegenTemp?: number;
+    hpRegenDuration?: number;
+    manaRaw?: number;
+    manaRegenPermanent?: number;
+    manaRegenTemp?: number;
+    manaRegenDuration?: number;
+    armorRaw?: number;
+    dmgRaw?: number;
+    dmgTempValue?: number;
+    dmgTempDuration?: number;
+    attSpeedRaw?: number;
+    armorDebuff?: number;
+    armorDebuffDuration?: number;
+    attSpeedDebuff?: number;
+    attSpeedDebuffDuration?: number;
+    magiceDmgTempValue?: number;
+    magicDmgTempDuration?: number;
+  };
+}
