71df75dc6d2d2ca2549ab705628e1f0a313097c3
Mohetti <moritz.hettich@gmx.de>
2021-08-30 16:54:01 +0300

    implemented a first draft of drag/drop items. This only works for the items of the hero  until now and doesnt update gold.

--- client/src/Components/GameState.ts
+++ client/src/Components/GameState.ts
@@ -1,13 +1,21 @@
 import { dataContainer } from '../index';
-import { HeroValues } from '../Models/responseModels';
+import {
+  HeroValues,
+  HeroValuesChild,
+  ItemValuesChild,
+} from '../Models/responseModels';
+import { itemStats } from '../Models/heroStartItems/startItems';
 
 export class GameState {
   heroObj;
   heroItems;
-  currentOpponent;
-  opponentItems;
+  currentOpponent!: HeroValuesChild;
+  opponentItems!: ItemValuesChild[];
   gameMode;
   heroKeys;
+  startItems!: ItemValuesChild[];
+  heroGold!: number;
+  opponentGold!: number;
 
   constructor(heroId: string, gameMode: string) {
     this.gameMode = gameMode;
@@ -16,25 +24,59 @@ export class GameState {
     this.heroObj = dataContainer.heroes.plainHeroObj(heroId);
     this.heroKeys = Object.keys(dataContainer.heroes.list as HeroValues);
 
-    // get current Opponent
-    const randomIndex = Math.floor(Math.random() * this.heroKeys.length);
-    this.currentOpponent! = dataContainer.heroes.plainHeroObj(
-      this.heroKeys[randomIndex].toString()
-    );
-
     // get hero items
     const itemKeys = Object.values(
       dataContainer.heroStartItems[this.heroObj['id']]
     );
     this.heroItems = dataContainer.items.plainItemObj(itemKeys);
+    this.setGold(this.heroItems, 'heroGold');
 
-    // get opponent items
+    this.getCurrentOpponent();
+    this.getOpponentItems();
+    this.getStartItems();
+  }
+
+  setHeroItems(newItem: string, oldItem: string) {
+    let list = [];
+    this.heroItems.map((x) => {
+      list.push(+x.id);
+    });
+    let index = this.heroItems.findIndex((x) => {
+      return x.id === +oldItem;
+    });
+
+    list[index] = +newItem;
+    this.heroItems = dataContainer.items.plainItemObj(list);
+  }
+
+  getCurrentOpponent() {
+    const randomIndex = Math.floor(Math.random() * this.heroKeys.length);
+    this.currentOpponent! = dataContainer.heroes.plainHeroObj(
+      this.heroKeys[randomIndex].toString()
+    );
+    // remove opp from list of keys
+    this.heroKeys.splice(randomIndex, 1);
+  }
+
+  getOpponentItems() {
     const oppItemKeys = Object.values(
       dataContainer.heroStartItems[this.currentOpponent['id']]
     );
     this.opponentItems = dataContainer.items.plainItemObj(oppItemKeys);
+    this.setGold(this.opponentItems, 'opponentGold');
+  }
 
-    // remove opp from list of keys
-    this.heroKeys.splice(randomIndex, 1);
+  getStartItems() {
+    const itemKeys = Object.keys(itemStats);
+    this.startItems = dataContainer.items.plainItemObj(itemKeys);
+  }
+
+  setGold(items: ItemValuesChild[], target: 'heroGold' | 'opponentGold') {
+    let gold = 0;
+    items.map((x) => {
+      gold += itemStats[x['id']].gold;
+    });
+
+    this[target] = gold;
   }
 }
--- client/src/Components/GameView.ts
+++ client/src/Components/GameView.ts
@@ -1,40 +1,150 @@
 import { Component } from './Component';
 import { dataContainer } from '../index';
+import { ItemValuesChild } from '../Models/responseModels';
+import { autobind } from '../Decorators/autobind';
+import { Draggable, DragTarget } from '../Models/eventlisteners';
+import { itemStats } from '../Models/heroStartItems/startItems';
 
-export class GameView extends Component<HTMLDivElement, [HTMLDivElement]> {
+export class GameView
+  extends Component<HTMLDivElement, [HTMLDivElement]>
+  implements Draggable, DragTarget
+{
   static instance: GameView;
+  gameContainer = this.element[0].firstElementChild!.children;
+  startItemsContainer = this.element[0].children[2];
 
   constructor() {
     super('tmpl-game-view', 'app');
-    this.render();
+    this.renderHero();
+    this.renderOpponent();
+    this.renderStartItems();
     this.dispatch();
     this.attach(true);
+    this.configureEventListeners();
   }
 
-  render() {
-    const heroContainer = this.element[0].firstElementChild!.children[0];
-    const opponentContainer = this.element[0].firstElementChild!.children[2];
-    // Hero and Opponent Img Loading
-    (<HTMLImageElement>heroContainer.children[0]).src =
-      dataContainer.gameState.heroObj['img'];
-    (<HTMLImageElement>opponentContainer.children[0]).src =
-      dataContainer.gameState.currentOpponent['img'];
+  renderItems(source: ItemValuesChild[], target: Element) {
+    const items = Object.values(source);
 
-    // Loading items
-    const heroItems = Object.values(dataContainer.gameState.heroItems);
-    heroItems.map((x) => {
+    while (target.firstChild) {
+      target.removeChild(<HTMLElement>target.lastChild);
+    }
+
+    items.map((x) => {
       const img = document.createElement('img');
       img.src = x['img'];
-      heroContainer.children[1].appendChild(img);
-    });
-
-    const opponentItems = Object.values(dataContainer.gameState.opponentItems);
-    opponentItems.map((x) => {
-      const img = document.createElement('img');
-      img.src = x['img'];
-      opponentContainer.children[1].appendChild(img);
+      img.id = x['id'].toString();
+      target.appendChild(img);
       return;
     });
+  }
+
+  renderStartItems() {
+    this.renderItems(
+      dataContainer.gameState.startItems,
+      this.startItemsContainer
+    );
+  }
+
+  renderGold(source: number, target: Element) {
+    target.textContent = (600 - source).toString() + ' Gold left';
+  }
+
+  renderHero() {
+    const heroContainer = this.gameContainer[0];
+    (<HTMLImageElement>heroContainer.children[0]).src =
+      dataContainer.gameState.heroObj['img'];
+    this.renderItems(
+      dataContainer.gameState.heroItems,
+      heroContainer.children[1]
+    );
+    this.renderGold(
+      dataContainer.gameState.heroGold,
+      heroContainer.children[2]
+    );
+  }
+
+  renderOpponent() {
+    const opponentContainer = this.gameContainer[2];
+    (<HTMLImageElement>opponentContainer.children[0]).src =
+      dataContainer.gameState.currentOpponent['img'].toString();
+    this.renderItems(
+      dataContainer.gameState.opponentItems,
+      opponentContainer.children[1]
+    );
+    this.renderGold(
+      dataContainer.gameState.opponentGold,
+      opponentContainer.children[2]
+    );
+  }
+
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData('text/plain', (<HTMLElement>event.target).id);
+    event.dataTransfer!.effectAllowed = 'copy';
+    console.log('start');
+  }
+
+  @autobind
+  dragEndHandler(_: DragEvent) {}
+  @autobind
+  dragOverHandler(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  @autobind
+  dragLeaveHandler(event: DragEvent) {}
+
+  @autobind
+  dropHandler(event: DragEvent) {
+    event.preventDefault();
+    const itemId = event.dataTransfer!.getData('text/plain');
+    const gold =
+      dataContainer.gameState.heroGold +
+      itemStats[itemId].gold -
+      itemStats[(<HTMLElement>event.target!).id].gold;
+    if (gold < 601) {
+      dataContainer.gameState.setHeroItems(
+        itemId,
+        (<HTMLElement>event.target!).id
+      );
+
+      const heroContainer = this.gameContainer[0];
+
+      this.renderItems(
+        dataContainer.gameState.heroItems,
+        heroContainer.children[1]
+      );
+      /*
+    this.renderGold(
+      dataContainer.gameState.heroGold,
+      heroContainer.children[2]
+    );
+      */
+    }
+  }
+
+  private configureEventListeners() {
+    (<HTMLInputElement>this.startItemsContainer).addEventListener(
+      'dragstart',
+      this.dragStartHandler
+    );
+    (<HTMLInputElement>this.startItemsContainer).addEventListener(
+      'dragend',
+      this.dragEndHandler
+    );
+    (<HTMLInputElement>this.gameContainer[0].children[1]).addEventListener(
+      'dragover',
+      this.dragOverHandler
+    );
+    (<HTMLInputElement>this.gameContainer[0].children[1]).addEventListener(
+      'dragleave',
+      this.dragLeaveHandler
+    );
+    (<HTMLInputElement>this.gameContainer[0].children[1]).addEventListener(
+      'drop',
+      this.dropHandler
+    );
   }
 
   static getInstance() {
