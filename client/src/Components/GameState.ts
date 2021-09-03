0f40801b23f63975febfb5727518308eefc02fd1
Mohetti <moritz.hettich@gmx.de>
2021-09-03 10:40:38 +0300

    implemented recursion function to calculate the winner of a matchup. Carefully needs to be reexaminied for the math and then being refactored in more readable components. Also some of the variable names are confusing since the function that gets called each recursion cycle reads as it would only matter for enemy, but it's in fact a generic function for both calculating the new herostats and the new enemystats. Will address that in the refactor.  As of the base structure: The function timeTillWin get the predefined objects of Components Calculation and starts the recursion. In the global function scope, the function for the actual calculation gets called twice. Once for hero, once for the enemy.It takes the heroObj and enemyObj as parameters. Couldn't do it with only 1 parameter since damage stats are dependant on items the other minion has. The two functions return new object that are similar to the ones from Calculation, but with new values for -undependent- health and -dependent on consumable items that are triggered on certain thresholds- damage, healthRegen and heal. The 2 new objects are used to recall the recursive function. The if statements acccount for health < 0 combined with the time it takes to delete the opponent. The time is needed here since the returned time from the recursion cycle for both objects is not equal, but based on the attacktime it takes until the enemy is at 0 health. That most of the time is different between both enemy and hero thrown into the function.

--- client/src/Components/GameState.ts
+++ client/src/Components/GameState.ts
@@ -6,6 +6,7 @@ import {
 } from '../Models/responseModels';
 import { itemStats } from '../Models/heroStartItems/startItems';
 import { HeroStatsForCalculation } from './Calculation';
+import { ValuesForCalculation } from './Calculation';
 
 export class GameState {
   heroObj: HeroValuesChild;
@@ -61,8 +62,134 @@ export class GameState {
     return goldCount < 601;
   }
 
+  calcCycle(ownStats: ValuesForCalculation, enemyStats: ValuesForCalculation) {
+    if (enemyStats.health <= 0) {
+      return enemyStats;
+    }
+    let enemyDmgBlock;
+    let _enemyArmor = enemyStats.armor - ownStats.armorDebuff;
+    let _dmgBlock = (0.052 * _enemyArmor) / (0.9 + 0.048 * _enemyArmor);
+    _enemyArmor < 0
+      ? (enemyDmgBlock = 1 + _dmgBlock)
+      : (enemyDmgBlock = 1 - _dmgBlock);
+
+    let dmgPerHit;
+    enemyStats.isMelee
+      ? (dmgPerHit = ownStats.damage * enemyDmgBlock - 8)
+      : (dmgPerHit = ownStats.damage * enemyDmgBlock);
+
+    // first damage cycle
+    let attacksNeeded =
+      enemyStats.health / dmgPerHit < 1
+        ? 1
+        : Math.floor(enemyStats.health / dmgPerHit);
+    let hpLeft = enemyStats.health - dmgPerHit * attacksNeeded;
+    let timeItTakes = ownStats.atkPerSec * attacksNeeded;
+    console.log(timeItTakes);
+
+    hpLeft = hpLeft - Math.floor(timeItTakes) * ownStats.damageTemp;
+
+    // health regen
+    let hpNew!: number;
+    if (enemyStats.tempHealthRegenDuration === 0) {
+      let healthRegened = timeItTakes * enemyStats.healthRegen;
+      hpNew = hpLeft + healthRegened;
+    }
+    if (timeItTakes <= enemyStats.tempHealthRegenDuration) {
+      let healthRegened = timeItTakes * enemyStats.healthRegen;
+      hpNew = hpLeft + healthRegened;
+      enemyStats.tempHealthRegenDuration += -Math.floor(timeItTakes);
+    }
+
+    if (
+      timeItTakes > enemyStats.tempHealthRegenDuration &&
+      enemyStats.tempHealthRegenDuration !== 0
+    ) {
+      let healthRegened;
+      let restRegen = enemyStats.tempHealthRegenDuration * 7.1875;
+      enemyStats.tempHealthRegenDuration = 0;
+      enemyStats.healthRegen += -7.1875;
+      healthRegened = restRegen + enemyStats.healthRegen * timeItTakes;
+      hpNew = hpLeft + healthRegened;
+    }
+
+    if (hpNew < enemyStats.health * 0.15) {
+      if (enemyStats.heal > 0) {
+        hpNew += 85;
+        enemyStats.heal += -85;
+        enemyStats.damage += -2;
+      }
+    }
+    enemyStats.health = hpNew;
+    enemyStats.time += timeItTakes;
+
+    return enemyStats;
+  }
+
+  timeTillWin(
+    ownStats: ValuesForCalculation,
+    enemyStats: ValuesForCalculation
+  ): any {
+    let enemy = this.calcCycle(ownStats, enemyStats);
+    let own = this.calcCycle(enemyStats, ownStats);
+
+    if (own.health > 0 && enemy.health > 0) {
+      return this.timeTillWin(own, enemy);
+    }
+
+    if (own.health < 0 && enemy.health > 0 && own.time < enemy.time) {
+      console.log(own.health + ' hero health');
+      console.log(own.time + ' hero time');
+      console.log(enemy.health + ' enemy health');
+      console.log(enemy.time + ' enemy time');
+      return alert('You lost!');
+    }
+
+    if (own.health > 0 && enemy.health < 0 && own.time > enemy.time) {
+      console.log(own.health + ' hero health');
+      console.log(own.time + ' hero time');
+      console.log(enemy.health + ' enemy health');
+      console.log(enemy.time + ' enemy time');
+      return alert('You won!');
+    }
+
+    if (own.health < 0 && enemy.health > 0 && own.time > enemy.time) {
+      return this.timeTillWin(own, enemy);
+    }
+
+    if (own.health > 0 && enemy.health < 0 && own.time < enemy.time) {
+      return this.timeTillWin(own, enemy);
+    }
+
+    if (own.health < 0 && enemyStats.health < 0) {
+      console.log(own.health + ' hero health');
+      console.log(own.time + ' hero time');
+      console.log(enemy.health + ' enemy health');
+      console.log(enemy.time + ' enemy time');
+      return own.time < enemy.time ? alert('You won!') : alert('You lost!');
+    }
+
+    // if ownStats hp < 0 && enemyStats hp > 0 && timeItTakes > timeItTakes return enemy Winner
+
+    // if ownStats hp > 0 && enemyStats hp < 0 && timeItTakes < timeItTakes return own Winner
+
+    // if ownStats hp < 0 && enemStats hp > 0 && timeItTakes < timeItTakes calc timeTillEnemyDies keep timeTillOwnDies
+
+    // if ownStats hp > 0 && enemyStats hp < 0 && timeItTakes > timeItTakes calc timeTillOwnDies keep timeTillEnemyDies
+
+    // if ownStats hp < 0 && enemyStats hp < 0
+
+    // heal > 0 - 2 dmg when used
+    // dmgTemp => for each sec deal 2 dmg ontop
+  }
+
   performCalculation() {
-    HeroStatsForCalculation(this.heroObj, this.heroItems);
+    const heroStats = HeroStatsForCalculation(this.heroObj, this.heroItems);
+    const opponentStats = HeroStatsForCalculation(
+      this.currentOpponent,
+      this.opponentItems
+    );
+    this.timeTillWin(heroStats, opponentStats);
   }
 
   private setOpponentItems(newItem: string, oldItem: string) {
