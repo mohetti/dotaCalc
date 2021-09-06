ae9cf350e9f93a6be1706bf122b2fd6c99ee4f84
Mohetti <moritz.hettich@gmx.de>
2021-09-06 16:27:09 +0300

    initial dist subtree commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
